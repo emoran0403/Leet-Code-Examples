ce793022a6266a440620e74fe805ada4a91feb0d
Eric Moran <emoran0403@gmail.com>
2022-09-22 14:17:06 -0400

    finished who likes it, made file for missing letter

---
+++ CodeExamples/CodeWars/FindTheMissingLetter.js
@@ -0,0 +1,8 @@
+//@  https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/javascript
+
+// ["a","b","c","d","f"] -> "e"
+// ["O","Q","R","S"] -> "P"
+
+function findMissingLetter(array) {
+  return " ";
+}
--- CodeExamples/CodeWars/WhoLikesIt.js
+++ CodeExamples/CodeWars/WhoLikesIt.js
@@ -1,3 +1,36 @@
 function likes(names) {
-  // TODO
+  // evaluate the length of the given names, and conditionally return based on the length
+  if (names.length === 0) {
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  // evaluate the length of the given names, and conditionally return based on the length
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
 }
