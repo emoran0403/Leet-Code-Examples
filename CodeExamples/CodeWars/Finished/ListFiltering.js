42b3178a9edaec18b26023d8424e8358f9d1aa66
Eric Moran <emoran0403@gmail.com>
2022-09-29 15:12:53 -0400

    finished some examples, renamed the incomplete examples

---
+++ CodeExamples/CodeWars/Finished/ListFiltering.js
@@ -0,0 +1,14 @@
+//@ https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+function filter_list(l) {
+  // declare an array to return, which will have only numbers pushed into it
+  let returnArray = [];
+  // iterate over the array, checking the type of data for each element
+  l.forEach((element) => {
+    // if it is a number type, keep it, if not, then discard it
+    if (typeof element === "number") {
+      returnArray.push(element);
+    }
+  });
+  return returnArray;
+}
---
+++ CodeExamples/CodeWars/Finished/SumOfFirstNthTerms.js
@@ -0,0 +1,31 @@
+//@ https://www.codewars.com/kata/555eded1ad94b00403000071/train/javascript
+
+function SeriesSum(n) {
+  // handle edge cases
+  if (n === 1) {
+    return `1.00`;
+  }
+  if (n === 0) {
+    return `0.00`;
+  }
+  //* generate each term 1-n, pushing them into an array
+  let series = [];
+  for (let i = 1; i <= n; i++) {
+    // calculate the term...
+    const term = 1 / (3 * i - 2);
+    // ... and push the term into the series array
+    series.push(term);
+    // console.log({ series });
+  }
+
+  // sum each term
+  const value = series.reduce((a, b) => a + b, 0);
+  const rounded = Math.round(value * 100) / 100;
+  //   console.log({ value });
+
+  // return the rounded result as a decimal with 2 decimal places
+  // Number.toFixed(x) ensures there are x digits to the right of the decimal, adding zeros as needed
+  return `${rounded.toFixed(2)}`;
+}
+
+// SeriesSum(4);
