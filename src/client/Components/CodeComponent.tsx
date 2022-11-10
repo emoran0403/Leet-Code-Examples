b65bb57f39b1fe502eb664106524d8c632ea4158
Eric Moran <emoran0403@gmail.com>
2022-11-10 17:09:25 -0500

    added height and overflow styling to code components to allow long code blocks to expand, and disallow small code blocks from expanding

--- src/client/Components/CodeComponent.tsx
+++ src/client/Components/CodeComponent.tsx
@@ -1,13 +1,30 @@
 import * as React from "react";
+import { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { tomorrowNightBright } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import * as Types from "../../../Types";
 
 const CodeComponent = (props: Types.CodeComponent) => {
-  console.log(props.codeString);
+  //lines of code
+  const lines = props.codeString.split("\n").length;
+  console.log(lines);
+
+  // set the toggle based on whether or not there are more than 10 lines of code
+  const [height, setHeight] = useState<"225.6px" | "auto">(lines > 10 ? "225.6px" : "auto");
+  const [overflowY, setOverflowY] = useState<"scroll" | "clip">(lines > 10 ? "scroll" : "clip");
+
+  const handleScroller = () => {
+    // if the code is short and does not need a scroll bar, dont allow it to get one
+
+    if (lines > 10) {
+      setHeight(height === "225.6px" ? "auto" : "225.6px");
+      setOverflowY(overflowY === "scroll" ? "clip" : "scroll");
+    }
+  };
+
   return (
-    <div>
-      <SyntaxHighlighter language="javascript" style={tomorrowNightBright}>
+    <div style={{ minHeight: height, height: height, overflowY: overflowY }} onClick={handleScroller}>
+      <SyntaxHighlighter style={tomorrowNightBright} language="javascript">
         {props.codeString}
       </SyntaxHighlighter>
     </div>
